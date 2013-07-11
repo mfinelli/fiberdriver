dc6732d335991ff0bd81e3f47f7f216ae6985ea5
Mario Finelli <mfinelli@server.fake>
2013-07-11 18:11:09 -0400

    check the OS and ask the user if correct

--- install.sh
+++ install.sh
@@ -14,7 +14,26 @@ function software_not_found {
 	exit 2
 }
 
+# ask the user to confirm something
+function confirm {
+	while read answer; do
+		case $answer in
+			YES|YEs|YeS|Yes|yES|yEs|yeS|yes|Y|y ) return 0 ;;
+			NO|No|nO|no|N|n ) return 1 ;;
+			* ) echo -n "Please enter yes or no: " ;;
+		esac
+	done
+}
+
 # make sure we have required software for installation
 hash cat 2>/dev/null || software_not_found "cat"
 hash grep 2>/dev/null || software_not_found "grep"
 hash awk 2>/dev/null || software_not_found "awk"
+
+# error if the os-release doesn't exist
+[ -f /etc/os-release ] || { echo "Could not find OS information" 1>&2; exit 3; }
+
+# let's get the OS information and ask the user if it looks correct
+OS=$(cat /etc/os-release | grep '^PRETTY_NAME' | awk -F= '{ print $2 }')
+echo -n "You appear to be using ${OS}. Is this correct? "
+confirm || { echo "Installation aborted."; exit 1; }
