81f9063edc1a17cb9ff6308dff538d9305eafaa1
Mario Finelli <mfinelli@server.fake>
2013-07-13 11:22:19 -0400

    add foundation assets

--- web/index.php
+++ web/index.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
 		<title>Fiber Driver</title>
+		<link rel="stylesheet" href="css/fiberdriver.css" />
+		<script src="js/custom.modernizr.js"></script>
 	</head>
 	<body>
 		<h1>Fiber Driver Server Management</h1>
+		<script src="js/jquery.js"></script>
+		<script src="js/foundation.js"></script>
+		<script src="js/foundation.alerts.js"></script>
+		<script src="js/foundation.clearing.js"></script>
+		<script src="js/foundation.cookie.js"></script>
+		<script src="js/foundation.dropdown.js"></script>
+		<script src="js/foundation.forms.js"></script>
+		<script src="js/foundation.interchange.js"></script>
+		<script src="js/foundation.joyride.js"></script>
+		<script src="js/foundation.magellan.js"></script>
+		<script src="js/foundation.orbit.js"></script>
+		<script src="js/foundation.placeholder.js"></script>
+		<script src="js/foundation.reveal.js"></script>
+		<script src="js/foundation.section.js"></script>
+		<script src="js/foundation.tooltips.js"></script>
+		<script src="js/foundation.topbar.js"></script>
+		<script>
+			$(document).foundation();
+		</script>
 	</body>
 </html>
